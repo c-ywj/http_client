a45d2bd3c2bd9a42059fe102ad1880cdfc9c0697
c-ywj <cybc00@hotmail.com>
2017-02-13 17:10:43 +0000

    Adds HTTPS GET request function, with buffering

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -1,25 +1,29 @@
 var https = require("https");
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
+  /* Add your code here */
+
   https.get(requestOptions, function (response) {
+
     response.setEncoding('utf8');
 
     response.on('data', function (data) {
-      console.log('Chunk Received: ', data  + "\n");
+      var buf = new Buffer(data);
+      console.log(buf.toString());
     });
 
     response.on('end', function () {
-      console.log('Response stream complete');
+      console.log('Response stream completed!');
     });
 
   });
 
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
